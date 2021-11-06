1aac6796879194fac50145ed2eb95e7831817646
Christophe Manin <christophe.manin@rcf.fr>
2021-11-06 19:22:23 +0100

    #9 add post requests middleware

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.json());
+
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Objet créé !'
+  });
+});
+
 app.use('/api/stuff', (req, res, next) => {
   const stuff = [
     {
